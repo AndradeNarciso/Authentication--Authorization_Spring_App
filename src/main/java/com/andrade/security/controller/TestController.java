f56252567c19f90eb693fd81dbd79f31521a9c31
AndradeNarciso <andradegregorio277@gmail.com>
2026-01-21 17:15:34 +0200

    V1 migration done

---
+++ src/main/java/com/andrade/security/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.andrade.security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/test")
+@RestController
+public class TestController {
+
+    @GetMapping
+    public String test(){
+        return "Testing security";
+    }
+    
+}
